e184a4d0ce03984e0c6ae864bdf8f31dd6f3ed76
ikychant <iki.informasi@gmail.com>
2019-04-15 15:22:22 +0700

    v0.4.1: Creating accounts app.

--- django_boards/settings.py
+++ django_boards/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards',
+
     'widget_tweaks',
+
+    'accounts',
+    'boards',
 ]
 
 MIDDLEWARE = [
