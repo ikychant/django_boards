dc3cc36f78620145af64867d13ed363d8380467e
ikychant <iki.informasi@gmail.com>
2019-04-15 12:02:49 +0700

    v0.33: Modificating templates with base template and creating custom font css.

--- django_boards/settings.py
+++ django_boards/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+oid0e1eol!t#qovce*i*cun=$j#5=8-0gwp_x9&2b^5opt+*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
