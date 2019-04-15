e4f446dbfd51aa632c08bfa66d08da47ff9bd0cb
ikychant <iki.informasi@gmail.com>
2019-04-15 10:26:08 +0700

    v0.31: Paging pk based, testing and 404.

--- boards/models.py
+++ boards/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
@@ -13,7 +12,6 @@ class Topic(models.Model):
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete= models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete= models.CASCADE)
-
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
