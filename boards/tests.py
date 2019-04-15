1b2698ac4a156d1e4d025f285a9fdf900ab8a472
ikychant <iki.informasi@gmail.com>
2019-04-15 14:33:50 +0700

    v0.4.0: Implementing django-widget-tweaks and form testing.

--- boards/tests.py
+++ boards/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 
 class HomeTests(TestCase):
@@ -55,9 +56,6 @@ class NewTopicTests(TestCase):
         Board.objects.create(name='Django', description='Django board.')
         User.objects.create_user(username='john', email='john@doe.com', password='123')  # <- included this line here
 
-
-
-
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.get(url)
@@ -93,6 +91,12 @@ class NewTopicTests(TestCase):
         self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
 
+    def test_contains_form(self):  # <- new test
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
     def test_new_topic_invalid_post_data(self):
         '''
         Invalid post data should not redirect
@@ -100,7 +104,9 @@ class NewTopicTests(TestCase):
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
