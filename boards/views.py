213026434bb6b96160b358efd8c4d95795d52d54
ikychant <iki.informasi@gmail.com>
2019-04-16 17:39:37 +0700

    v0.6.2-0.4.7a: Improving list view and pagination in GCBV with tests.

--- boards/views.py
+++ boards/views.py
@@ -13,21 +13,18 @@ class BoardListView(ListView):
     context_object_name = 'boards'
     template_name = 'home.html'
 
-def board_topics(request, pk):
-    board = get_object_or_404(Board, pk=pk)
-    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(queryset, 3)
-    try:
-        topics = paginator.page(page)
-    except PageNotAnInteger:
-        # fallback to the first page
-        topics = paginator.page(1)
-    except EmptyPage:
-        # probably the user tried to add a page number
-        # in the url, so we fallback to the last page
-        topics = paginator.page(paginator.num_pages)
-    return render(request, 'topics.html', {'board': board, 'topics': topics})
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'topics.html'
+    paginate_by = 5
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+        return queryset
 
 @login_required
 def new_topic(request, pk):
