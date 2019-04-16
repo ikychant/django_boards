540b78d0017c8d970057a6c45ff83b81ab519fea
ikychant <iki.informasi@gmail.com>
2019-04-16 21:01:58 +0700

    v0.6.4: Adding edit account and markdown features.

--- boards/models.py
+++ boards/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
+from django.utils.html import mark_safe
+from markdown import markdown
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
     def __str__(self):
         return self.name
-
     def get_posts_count(self):
         return Post.objects.filter(topic__board=self).count()
     def get_last_post(self):
-
         return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 class Topic(models.Model):
@@ -33,3 +33,5 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
