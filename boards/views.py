26c7a5254d8696e32162763916510e8b6afe3946
ikychant <iki.informasi@gmail.com>
2019-04-16 07:49:22 +0700

    v0.5.0-0.4.7a: Protecting views and login required testing.

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import NewTopicForm
-from .models import Board, Topic, Post
+from .models import Board, Post
 
 def home(request):
     boards = Board.objects.all()
@@ -11,20 +12,20 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user  # <- here
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user  # <- and here
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
